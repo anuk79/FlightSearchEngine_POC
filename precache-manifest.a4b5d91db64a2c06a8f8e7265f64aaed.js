43698133a8764c5b58e8068c5b42d95e311dab6e
Anuradha Kumari <anuradha.kumari@CaptainMarvel.local>
2019-12-21 21:53:17 +0530

    Updates

---
+++ precache-manifest.a4b5d91db64a2c06a8f8e7265f64aaed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "495955bf4f05936aa1cb7386cd6a8280",
+    "url": "/FlightSearchEngine_POC/index.html"
+  },
+  {
+    "revision": "54bbb2823c344333c7f5",
+    "url": "/FlightSearchEngine_POC/static/css/main.3d3216e0.chunk.css"
+  },
+  {
+    "revision": "d58832cb1d21af94265a",
+    "url": "/FlightSearchEngine_POC/static/js/2.d37f891d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/FlightSearchEngine_POC/static/js/2.d37f891d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "54bbb2823c344333c7f5",
+    "url": "/FlightSearchEngine_POC/static/js/main.b2789a71.chunk.js"
+  },
+  {
+    "revision": "2790a8e19bd21d7a022a",
+    "url": "/FlightSearchEngine_POC/static/js/runtime-main.846e352e.js"
+  }
+]);
